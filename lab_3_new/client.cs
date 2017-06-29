fadc7616c9e3ee2f24187181ca792df8203484eb
Dmitry <tvark7022@users.noreply.github.com>
2017-06-29 21:37:27 +0300

    Add files via upload

---
+++ lab_3_new/client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace Лабораторная_работа_4_ZeroMQ_Клиент
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZContext context = new ZContext();
+            ZSocket socket = new ZSocket(context, ZSocketType.PULL);
+            socket.Connect("tcp://192.168.1.43:5822");
+            while (true) {
+                ZFrame frame = socket.ReceiveFrame();
+                Console.WriteLine(frame.ReadString());
+            }
+        }
+    }
+}
---
+++ lab_3_new/server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace Лабораторная_работа_4_ZeroMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZContext context = new ZContext();
+            ZSocket socket = new ZSocket(context, ZSocketType.PUSH);
+            socket.Bind("tcp://192.168.1.43:5822");
+            Console.WriteLine("Сервер запущен.");
+            while (true) {
+                Console.Write("> ");
+                string message = Console.ReadLine();
+                socket.SendFrame(new ZFrame(message));
+            }
+        }
+    }
+}
